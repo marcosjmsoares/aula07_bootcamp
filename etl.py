23e7c244babae84731515c213aff392836af7b90
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-09 17:51:27 -0300

    adicionado funcao 1

---
+++ etl.py
@@ -0,0 +1,20 @@
+import csv
+path_arquivo = "vendas.csv"
+
+print("Iniciando ETL...")
+
+def ler_csv(nome_do_Arquivo_csv: str) -> list[dict]:
+    """
+    Funcao que le um arquivo csv e retorna uma lista de
+    dicionarios"""
+
+    lista = []
+    with open(nome_do_Arquivo_csv, mode="r", encoding='utf-8') as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for linha in leitor:
+            lista.append(linha)
+    return lista
+
+vendas_itens: list[dict]
+vendas_itens = ler_csv(path_arquivo)
+print(vendas_itens)
