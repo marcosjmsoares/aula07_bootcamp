098749c9daf0827b89132e49deb8c8a8afc989f9
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-12 17:42:21 -0300

    adicionado arquivos e funcionou funcao

--- exercicios_chamada funcao.py
+++ exercicios_chamada funcao.py
@@ -1,9 +1,15 @@
 # Exercícios
 # Vamos revisar funções adicionando type hints e Pydantic
+from exercicios_type_hint import ler_csv, calcular_media_dos_valores
 import csv
 
-# 1-Calcular Média de Valores em uma Lista
+path_arquivo = "vendas.csv"
 
+lista_de_produtos = ler_csv(path_arquivo)
+
+# 1-Calcular Média de Valores em uma Lista
+valor_medio = calcular_media_dos_valores(lista_de_produtos)
+print(valor_medio)
 
 # 2-Filtrar Dados Acima de um Limite
 
--- exercicios_type_hint.py
+++ exercicios_type_hint.py
@@ -1,9 +1,35 @@
 # Exercícios
 # Vamos revisar funções adicionando type hints e Pydantic
+from pydantic import BaseModel, field_validator, ValidationError
+from typing import List, Dict, Optional
 import csv
-from typing import List, Dict
+
+#####LER CSV
+def ler_csv(nome_do_Arquivo_csv: str) -> list[dict]:
+    """
+    Funcao que le um arquivo csv e retorna uma lista de
+    dicionarios"""
+
+    lista = []
+    with open(nome_do_Arquivo_csv, mode="r", encoding='utf-8') as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for linha in leitor:
+            lista.append(linha)
+    return lista
+
 
 # 1-Calcular Média de Valores em uma Lista
+def calcular_media_dos_valores(lista: list[dict]) -> float:
+    """
+    Funcao que calcula a media dos valores de uma lista
+    """
+    valor_media = 0
+    quantidade_itens = 0
+    for produto in lista:
+        valor_media += float(produto.get("preco"))
+        quantidade_itens += 1
+    return valor_media/quantidade_itens
+
 
 
 # 2-Filtrar Dados Acima de um Limite
